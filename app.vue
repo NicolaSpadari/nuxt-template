50b2d2ad70e075ef7db56ca1d2e5a5049365f7c1
NicolaSpadari <spadari.nicola@gmail.com>
2023-04-26 15:36:27 +0200

    Nuxtpage only in app

--- app.vue
+++ app.vue
@@ -1,26 +1,3 @@
 <template>
-    <v-app id="nuxtube">
-        <SiteNavbar />
-        <SiteSidebar />
-
-        <v-main>
-            <NuxtPage />
-        </v-main>
-    </v-app>
+    <NuxtPage />
 </template>
-
-<script lang="ts" setup>
-    const router = useRouter();
-    const user = useCurrentUser();
-    const { userChannel, getUserChannel } = useYoutube();
-
-    onMounted(() => {
-        watch(user, async (newUser, oldUser) => {
-            if (oldUser && !newUser) {
-                router.push("/");
-            } else if (newUser) {
-                await getUserChannel();
-            }
-        });
-    });
-</script>
